dfb2aef3a45219e2ad3d7ebbe88f2d0645ee2c51
landinjm <landinjm@umich.edu>
2026-03-25 13:57:30 -0400

    blah

--- main.cpp
+++ main.cpp
@@ -34,15 +34,21 @@
 #include <memory>
 
 int
-main()
+main(int argc, char* argv[])
 {
-  using VectorType = dealii::Vector<double>;
+  dealii::Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, 1);
+  MPI_Comm communicator = MPI_COMM_WORLD;
 
-  VectorType y(2);
-  VectorType y_dot(2);
+  using VectorType = dealii::TrilinosWrappers::MPI::Vector;
+
+  dealii::IndexSet locally_owned(2);
+  locally_owned.add_range(0, 2);
+
+  VectorType y, y_dot;
+  y.reinit(locally_owned, communicator);
+  y_dot.reinit(locally_owned, communicator);
 
   double kappa = 1.0;
-
   dealii::FullMatrix<double> A(2, 2);
   A(0, 1) = -1.0;
   A(1, 0) = kappa * kappa;
@@ -53,45 +59,65 @@ main()
   dealii::SUNDIALS::IDA<VectorType>::AdditionalData time_stepper_data;
   time_stepper_data.final_time = 10.0;
 
-  dealii::SUNDIALS::IDA<VectorType> time_stepper(time_stepper_data);
+  dealii::SUNDIALS::IDA<VectorType> time_stepper(time_stepper_data,
+                                                 communicator);
 
-  time_stepper.reinit_vector = [&](VectorType& v) { v.reinit(2); };
+  time_stepper.reinit_vector = [&](VectorType& v) {
+    v.reinit(locally_owned, communicator);
+  };
 
   time_stepper.residual = [&](const double t,
                               const VectorType& y,
                               const VectorType& y_dot,
-                              VectorType& residual) {
+                              VectorType& residual) -> int {
     residual = y_dot;
-    A.vmult_add(residual, y);
+    {
+      dealii::Vector<double> tmp_in(2), tmp_out(2);
+      for (unsigned int i = 0; i < 2; ++i)
+        tmp_in[i] = y[i];
+      A.vmult(tmp_out, tmp_in);
+      for (unsigned int i = 0; i < 2; ++i)
+        residual[i] += tmp_out[i];
+    }
+    return 0;
   };
 
   time_stepper.setup_jacobian = [&](const double t,
                                     const VectorType& y,
                                     const VectorType& y_dot,
-                                    const double alpha) {
+                                    const double alpha) -> int {
     J = A;
     J(0, 0) += alpha;
     J(1, 1) += alpha;
-
     J_inv.invert(J);
+    return 0;
   };
 
   time_stepper.solve_with_jacobian =
-    [&](const VectorType& src, VectorType& dst, const double tolerance) {
-      J_inv.vmult(dst, src);
-    };
+    [&](const VectorType& src, VectorType& dst, const double tolerance) -> int {
+    dealii::Vector<double> tmp_src(2), tmp_dst(2);
+    for (unsigned int i = 0; i < 2; ++i)
+      tmp_src[i] = src[i];
+    J_inv.vmult(tmp_dst, tmp_src);
+    for (unsigned int i = 0; i < 2; ++i)
+      dst[i] = tmp_dst[i];
+    return 0;
+  };
 
   time_stepper.output_step = [&](const double t,
                                  const VectorType& y,
                                  const VectorType& y_dot,
-                                 const unsigned int step_number) {
+                                 const unsigned int step_number) -> int {
     std::cout << "Step " << step_number << " time " << t << "\n";
+    return 0;
   };
 
   y[1] = kappa;
   y_dot[0] = kappa;
 
-  time_stepper.solve_dae(y, y_dot);
+  y.compress(dealii::VectorOperation::insert);
+  y_dot.compress(dealii::VectorOperation::insert);
 
+  time_stepper.solve_dae(y, y_dot);
   return 0;
 }
